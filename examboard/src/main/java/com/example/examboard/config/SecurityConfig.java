744ac795f440d95edb867f3c6ae5bceef075dc73
tjun17 <xowns3617@gmail.com>
2023-08-29 10:43:06 +0900

    #-9OAuth2 설정 파일
    1. application.yml에 kakao 설정 추가
    2.SecurityConfig 파일에 OAuth2 로그인 설정 추가

--- examboard/src/main/java/com/example/examboard/config/SecurityConfig.java
+++ examboard/src/main/java/com/example/examboard/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.examboard.config;
 
+import com.example.examboard.service.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,6 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    PrincipalOauth2UserService principalOauth2UserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -23,13 +28,19 @@ public class SecurityConfig {
 
                 .formLogin((form) -> form
                         .loginPage("/user/login")
-                        .loginProcessingUrl("/login")
+                        .loginProcessingUrl("/auth/login")
                         .usernameParameter("userId")
                         .defaultSuccessUrl("/articles/lists", true))
 
                 .logout(out -> out
                         .logoutSuccessUrl("/articles/lists")
                         .logoutUrl("/logout"))
+
+                .oauth2Login(oAuth -> oAuth
+                        //.loginPage("/loginForm")
+                        .defaultSuccessUrl("/articles/lists", true)
+                        .userInfoEndpoint(userInfo-> userInfo
+                                .userService(principalOauth2UserService)))
 
                 .csrf(csrf -> csrf.disable());
         return http.build();
